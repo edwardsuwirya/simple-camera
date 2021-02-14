b83b2cf1f9529372ca37c953409dc2ca388ad206
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-02-14 11:58:31 +0700

    Save Image to storage

--- app/src/main/java/com/enigmacamp/mycameraactivity/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/mycameraactivity/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.enigmacamp.mycameraactivity
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var photoImageView: ImageView
     private lateinit var takePictureButton: Button
+
+    lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +34,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun takePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
+        takePictureIntent.resolveActivity(packageManager)?.also {
+            val photoFile: File? = try {
+                createImageFile()
+            } catch (ex: IOException) {
+                null
+            }
+            photoFile?.also {
+                val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.enigmacamp.mycameraactivity.fileprovider",
+                        it
+                )
+                println(photoURI.toString())
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
     }
 
@@ -39,9 +59,31 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
+//            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val sourceBitmap =
+                    ImageDecoder.createSource(contentResolver, Uri.fromFile(File(currentPhotoPath)))
+            val imageBitmap = ImageDecoder.decodeBitmap(sourceBitmap)
             photoImageView.setImageBitmap(imageBitmap)
         }
 
+    }
+
+    private fun createImageFile(): File {
+
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+//        val storageDir: File? = getExternalFilesDir(DIRECTORY_PICTURES)
+        val imageDir = filesDir
+        val storageDir = File(imageDir, "photo_${timeStamp}.jpg")
+
+//        return File.createTempFile(
+//                "photo_${timeStamp}_",
+//                ".jpg",
+//                storageDir
+//        ).apply {
+//            currentPhotoPath = absolutePath
+//        }
+        return storageDir.apply {
+            currentPhotoPath = absolutePath
+        }
     }
 }
