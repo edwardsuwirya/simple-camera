9842b64b81cad771c05a4b2f4eac5637fd82bbcb
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-02-14 11:05:21 +0700

    Simple Android Kotlin Camera Intent

---
+++ app/src/main/java/com/enigmacamp/mycameraactivity/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.enigmacamp.mycameraactivity
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var photoImageView: ImageView
+    private lateinit var takePictureButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        photoImageView = findViewById(R.id.photo_imageView)
+        takePictureButton = findViewById(R.id.takePicture_button)
+        takePictureButton.setOnClickListener {
+            takePictureIntent()
+        }
+    }
+
+    private fun takePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            photoImageView.setImageBitmap(imageBitmap)
+        }
+
+    }
+}
